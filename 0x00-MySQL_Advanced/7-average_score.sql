82b59a98e48b19b965f238257f26f4d83c0619a3
Armolas <armolas06@gmail.com>
2024-06-16 00:35:14 +0100

    7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,19 +1,22 @@
--- 7-average_score.sql
+-- ComputeAverageScoreForUser.sql
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
 
-    -- Compute the average score for the user
+    -- Calculate the average score
     SELECT AVG(score) INTO avg_score
     FROM corrections
     WHERE user_id = user_id;
 
-    -- Update the user's average_score
+    -- Update the users table with the average score
     UPDATE users
     SET average_score = avg_score
     WHERE id = user_id;
-END//
+END //
 
 DELIMITER ;
