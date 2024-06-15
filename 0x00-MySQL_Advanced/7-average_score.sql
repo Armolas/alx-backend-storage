ea9f061c1f612690fd3b4a4ca2ffbc5087d0e3c1
Armolas <armolas06@gmail.com>
2024-06-16 00:55:26 +0100

    7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,12 +6,25 @@ DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
+    DECLARE total_score FLOAT;
+    DECLARE project_count INT;
     DECLARE avg_score FLOAT;
 
+    -- Calculate the total score and the number of projects for the user
+    SELECT 
+        SUM(score) INTO total_score, 
+        COUNT(*) INTO project_count
+    FROM 
+        corrections
+    WHERE 
+        user_id = user_id;
+
     -- Calculate the average score
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
+    IF project_count > 0 THEN
+        SET avg_score = total_score / project_count;
+    ELSE
+        SET avg_score = 0;
+    END IF;
 
     -- Update the users table with the average score
     UPDATE users
