21d54d6b9f3f4086d37919e6dbd717414dfe0efc
Armolas <armolas06@gmail.com>
2024-07-01 14:15:40 +0100

    task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 '''exercise task'''
+from functools import wraps
 import redis
 import uuid
 from typing import Union, Callable, Any
 
 
+def count_calls(method: Callable) -> Callable:
+    '''counts how many times the Cache class is called'''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''counts calls'''
+        key = f"{method.__qualname__}"
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 class Cache:
     '''This is a redis Cache class'''
     def __init__(self):
@@ -12,6 +22,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''stores a data in redis'''
         key = str(uuid.uuid1())
